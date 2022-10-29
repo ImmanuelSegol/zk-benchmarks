bef5ac613f3131b8ad943baa7587b5a304b5291f
ImmanuelSegol <3ditds@gmail.com>
2022-10-28 23:51:13 -0400

    feat

---
+++ platforms/rapidsnark.sh
@@ -0,0 +1,21 @@
+# <circuit.zkey> <witness.wtns> <proof.json> <public.json>
+if ! -d "./rapidsnark"
+then
+    git clone https://github.com/iden3/rapidsnark --recurse-submodules
+    cd rapidsnark
+
+    # install deps
+    sudo apt install build-essential
+    sudo apt-get install libgmp-dev
+    sudo apt-get install libsodium-dev
+    sudo apt-get install nasm
+
+    npm install
+    git submodule init
+    git submodule update
+    npx task createFieldSources
+    npx task buildRust
+    npx task buildProver
+fi
+
+
